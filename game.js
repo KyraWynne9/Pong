ab5f711a823dbe179bc572e51de85b053f177a56
KyraWynne9 <kewynne@email.neit.edu>
2026-04-13 01:06:11 -0400

    a

--- game.js
+++ game.js
@@ -8,26 +8,9 @@ dy = 6;
 
 var player = new GameObject();
 var ball = new GameObject(canvas.width/2,canvas.height/2);
-ball.vx = 0;
-ball.vy = 2;
+ball.vx = -6;
+ball.vy = 0;
 
-
-// function GameObject() {
-
-//     x += dx;
-//     y += dy;
-
-    
-//     if (s && player.y + player.height/2 < canvas.height) {
-//         player.y += 5;
-//     }
-//     if (w && player.y - player.height/2 > 0) {
-//         player.y -= 5;
-//     }
-
-//     player.move();
-//     player.drawRect();
-// }
 
 // function Ball() {
 
@@ -49,11 +32,35 @@ function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     // GameObject();
 
-    if(ball.x + ball.width/2 > canvas.width){
+    // player code
+
+     x += dx;
+     y += dy;
+
+    
+   if (s && player.y + player.height/2 < canvas.height) {
+        player.y += 5;
+    }
+     if (w && player.y - player.height/2 > 0) {
+         player.y -= 5;
+     }
+
+     player.move();
+     player.drawRect();
+
+    // ball code
+
+    if(ball.x + ball.width/2 < canvas.width){
         ball.vx = -ball.vx;
     }
     if(ball.y + ball.width/2 > canvas.height){
         ball.vy = -ball.vy;
+    }
+    if(ball.y - ball.width < -1.5){
+        ball.vy = -ball.vy;
+    }
+    if(ball.x - ball.width > player.width){
+        ball.vx = -ball.vx;
     }
 
     ball.x += ball.vx;
